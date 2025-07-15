6f9ca7a630c972fac4a236c8bce397cb85198b97
竹光 翼 <tsubasa.takemitsu.ni@ambl.group>
2025-07-15 18:21:33 +0900

    github actions 修正

--- .github/scripts/pr.js
+++ .github/scripts/pr.js
@@ -1,13 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 (async () => {
     try {
         const args = require('minimist')(process.argv.slice(3));
 
         const token = args['github-token'];
-        const github = args['github'];
         const featureBranch = args['feature-branch'];
+
+        const octokit = github.getOctokit(token);
+
+        const repoFull = args['repo'];
+        const [owner, repo] = repoFull.split('/');
+
+        await octokit.rest.pulls.update({
+            owner,
+            repo,
+            pull_number,
+            body: "test",
+          });
 
         console.log('✅ GitHub Token:', token ? '[REDACTED]' : '❌ Not provided');
         console.log('📌 Feature Branch:', featureBranch);
+        console.log('owner:', owner);
+        console.log('repo:', repo);
 
     }catch(err){
         console.log(err.message);
