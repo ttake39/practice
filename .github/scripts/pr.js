054db0074b7bd86bacd38f115af4fdd3188feb54
竹光 翼 <tsubasa.takemitsu.ni@ambl.group>
2025-07-17 15:05:51 +0900

    feat: js修正

--- .github/scripts/pr.js
+++ .github/scripts/pr.js
@@ -1,31 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-(async () => {
-    try {
-        const args = require('minimist')(process.argv.slice(3));
+/**
+ * script entry point (main処理)
+ * @param {object} args - CLI引数をパースしたオブジェクト
+ * @returns {Promise<void>}
+ */
+async function main(args){
 
-        const token = args['github-token'];
-        const featureBranch = args['feature-branch'];        
-        const repoFull = args['repo'];
-        const [owner, repo] = repoFull.split('/');
-        const pull_number = parseInt(args['pr-number'], 10);
+    const token = args['github-token'];
+    
+}
 
-        console.log('✅ GitHub Token:', token ? '[REDACTED]' : '❌ Not provided');
-        console.log('📌 Feature Branch:', featureBranch);
-        console.log('owner:', owner);
-        console.log('repo:', repo);
-
-        const octokit = github.getOctokit(token);
-
-        await octokit.rest.pulls.update({
-            owner,
-            repo,
-            pull_number,
-            body: "test",
-          });
-
-    }catch(err){
-        console.log(err.message);
-    }
-})();
+module.exports = { main };
